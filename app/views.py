3476ad8b0b9c7de112532b4994cb766952fe9cdb
TajNgaio <tajsivers@live.com>
2018-03-05 04:01:55 +0000

    Committed this already

--- app/views.py
+++ app/views.py
@@ -27,21 +27,31 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route('/secure-page')
+def secure_page():
+    @login_required
+    "Render the website's secure page"
+    return render_template('secure-page.html')
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
+        
+        id = form.id.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
         # change this to actually validate the entire form submission
         # and not just one field
         if form.username.data:
             # Get the username and password values from the form.
-
+            username = form.username.data
+            password = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
+            user = UserProfile.query.filter_by(username="TajSivers97", password="c0ldlike6")
             # get user id, load into session
             login_user(user)
 
@@ -49,6 +59,12 @@ def login():
             return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('home'))
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
